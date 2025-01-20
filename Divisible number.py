7b00acd1b9145e1cf29d6cef6186bf3830446a75
Tani-alt <Tanirika.gupta@gmail.com>
2025-01-20 21:33:14 +0530

    divisible numbers

---
+++ Divisible number.py
@@ -0,0 +1,7 @@
+#divisible numbers
+numn=int(input("Enter the number (Numerator)"))
+numd=int(input("Enter the number (Denominator)"))
+if numn%numd==0:
+   print("it is completly divisible")
+else :
+   print("it is not divisible")
